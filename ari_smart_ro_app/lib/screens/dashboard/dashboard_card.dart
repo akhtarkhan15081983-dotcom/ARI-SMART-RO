fa637193f90f665621fbe78b36cf12b6984e9a14
akhtarkhan15081983-dotcom <akhtar.khan15081983@gmail.com>
2026-08-20 00:58:24 +0530

    Wire referral dashboard card to referral screen

--- ari_smart_ro_app/lib/screens/dashboard/dashboard_card.dart
+++ ari_smart_ro_app/lib/screens/dashboard/dashboard_card.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+import '../customer/referral_screen.dart';
+
 class DashboardCard extends StatelessWidget {
   final String title;
   final IconData icon;
@@ -12,37 +14,41 @@ class DashboardCard extends StatelessWidget {
     required this.onTap,
   });
 
+  void _handleTap(BuildContext context) {
+    if (title == 'Referral') {
+      Navigator.of(context).push(
+        MaterialPageRoute(
+          builder: (_) => const ReferralScreen(),
+        ),
+      );
+      return;
+    }
+
+    onTap();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Card(
       elevation: 4,
-
       shape: RoundedRectangleBorder(
         borderRadius: BorderRadius.circular(15),
       ),
-
       child: InkWell(
         borderRadius: BorderRadius.circular(15),
-
-        onTap: onTap,
-
+        onTap: () => _handleTap(context),
         child: Column(
-          mainAxisAlignment:
-              MainAxisAlignment.center,
-
+          mainAxisAlignment: MainAxisAlignment.center,
           children: [
             Icon(
               icon,
               size: 50,
               color: Colors.blue,
             ),
-
             const SizedBox(height: 15),
-
             Text(
               title,
               textAlign: TextAlign.center,
-
               style: const TextStyle(
                 fontWeight: FontWeight.bold,
               ),
