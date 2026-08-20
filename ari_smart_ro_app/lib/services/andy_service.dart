bbdedb4e71f2ac5b43fa86aa2e3d08e10fd21d5d
akhtarkhan15081983-dotcom <akhtar.khan15081983@gmail.com>
2026-08-20 23:29:19 +0530

    Add Flutter upload to ANDY local speech recognition

--- ari_smart_ro_app/lib/services/andy_service.dart
+++ ari_smart_ro_app/lib/services/andy_service.dart
@@ -9,24 +9,39 @@ class AndyReply {
   const AndyReply({required this.conversationId, required this.messageId, required this.answer});
 }
 
+class AndyTranscription {
+  final String text;
+  final String? language;
+  final double languageProbability;
+  const AndyTranscription({required this.text, this.language, required this.languageProbability});
+}
+
 class AndyService {
   Future<AndyReply> chat(String message, {int? conversationId}) async {
     final response = await http.post(
       Uri.parse('${ApiService.baseUrl}/andy/chat/'),
       headers: await ApiService.authHeaders(),
-      body: jsonEncode({
-        'message': message,
-        if (conversationId != null) 'conversation_id': conversationId,
-      }),
+      body: jsonEncode({'message': message, if (conversationId != null) 'conversation_id': conversationId}),
     );
     final data = response.body.isEmpty ? <String, dynamic>{} : Map<String, dynamic>.from(jsonDecode(response.body));
-    if (response.statusCode != 200) {
-      throw Exception(data['message']?.toString() ?? 'ANDY is unavailable');
-    }
-    return AndyReply(
-      conversationId: data['conversation_id'] as int,
-      messageId: data['message_id'] as int,
-      answer: data['answer']?.toString() ?? '',
+    if (response.statusCode != 200) throw Exception(data['message']?.toString() ?? 'ANDY is unavailable');
+    return AndyReply(conversationId: data['conversation_id'] as int, messageId: data['message_id'] as int, answer: data['answer']?.toString() ?? '');
+  }
+
+  Future<AndyTranscription> transcribe(String audioPath) async {
+    final headers = await ApiService.authHeaders();
+    final request = http.MultipartRequest('POST', Uri.parse('${ApiService.baseUrl}/andy/transcribe/'));
+    final authorization = headers['Authorization'];
+    if (authorization != null) request.headers['Authorization'] = authorization;
+    request.files.add(await http.MultipartFile.fromPath('audio', audioPath));
+    final streamed = await request.send();
+    final response = await http.Response.fromStream(streamed);
+    final data = response.body.isEmpty ? <String, dynamic>{} : Map<String, dynamic>.from(jsonDecode(response.body));
+    if (response.statusCode != 200) throw Exception(data['message']?.toString() ?? 'Voice recognition failed');
+    return AndyTranscription(
+      text: data['text']?.toString() ?? '',
+      language: data['language']?.toString(),
+      languageProbability: (data['language_probability'] as num?)?.toDouble() ?? 0,
     );
   }
 
