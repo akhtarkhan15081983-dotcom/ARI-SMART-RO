178990fe0449e163efc9c01589a400797b015acd
akhtarkhan15081983-dotcom <akhtar.khan15081983@gmail.com>
2026-08-20 16:52:34 +0530

    Expose face enrollment endpoint

--- backend/employees/urls.py
+++ backend/employees/urls.py
@@ -6,38 +6,38 @@ from .views import (
     EmployeeProfileAPIView,
     EngineerListAPIView,
     AssignmentEmployeeListAPIView,
+    FaceEnrollmentAPIView,
 )
 
 urlpatterns = [
-
     path(
         "employees/live-location/",
         UpdateLiveLocationAPIView.as_view(),
         name="live-location",
     ),
-
     path(
         "employees/live-map/",
         EngineerLiveMapAPIView.as_view(),
         name="live-map",
     ),
-
     path(
         "employees/profile/",
         EmployeeProfileAPIView.as_view(),
         name="employee-profile",
     ),
-
+    path(
+        "employees/face-enrollment/",
+        FaceEnrollmentAPIView.as_view(),
+        name="face-enrollment",
+    ),
     path(
         "employees/engineers/",
         EngineerListAPIView.as_view(),
         name="engineer-list",
     ),
-
     path(
         "employees/assignment-employees/",
         AssignmentEmployeeListAPIView.as_view(),
         name="assignment-employees",
     ),
-
 ]
