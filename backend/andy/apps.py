6d78a45636dd62fcaf24f513f26cea8b0059dd67
akhtarkhan15081983-dotcom <akhtar.khan15081983@gmail.com>
2026-08-20 22:01:54 +0530

    Start ANDY self-hosted assistant backend

---
+++ backend/andy/apps.py
@@ -0,0 +1,7 @@
+from django.apps import AppConfig
+
+
+class AndyConfig(AppConfig):
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "andy"
+    verbose_name = "ANDY AI Assistant"
