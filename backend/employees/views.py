427eea2dc16a1720803c895aadd20127cba0310b
akhtarkhan15081983-dotcom <akhtar.khan15081983@gmail.com>
2026-09-20 17:36:18 +0530

    Add career movement parsing imports

--- backend/employees/views.py
+++ backend/employees/views.py
@@ -1,3 +1,5 @@
+from datetime import date
+from decimal import Decimal
 from django.utils import timezone
 from django.db import transaction
 from django.contrib.auth.password_validation import validate_password
