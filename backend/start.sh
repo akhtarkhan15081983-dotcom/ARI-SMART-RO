63b286588901ee155f14b844a67e8c3710ff1d61
akhtarkhan15081983-dotcom <akhtar.khan15081983@gmail.com>
2026-09-23 23:42:12 +0530

    Reduce production memory pressure in gunicorn

--- backend/start.sh
+++ backend/start.sh
@@ -8,7 +8,9 @@ python manage.py collectstatic --noinput
 
 exec gunicorn config.wsgi:application \
     --bind "0.0.0.0:${PORT:-8000}" \
-    --workers "${GUNICORN_WORKERS:-3}" \
+    --workers "${GUNICORN_WORKERS:-2}" \
     --timeout "${GUNICORN_TIMEOUT:-120}" \
+    --max-requests "${GUNICORN_MAX_REQUESTS:-750}" \
+    --max-requests-jitter "${GUNICORN_MAX_REQUESTS_JITTER:-100}" \
     --access-logfile - \
     --error-logfile -
