c274ab8a6e9c739957d2180e1c6d7c654733f669
akhtarkhan15081983-dotcom <akhtar.khan15081983@gmail.com>
2026-09-26 11:49:04 +0530

    security: load session revocation signals

--- backend/accounts/apps.py
+++ backend/accounts/apps.py
@@ -2,4 +2,7 @@ from django.apps import AppConfig
 
 
 class AccountsConfig(AppConfig):
-    name = 'accounts'
+    name = "accounts"
+
+    def ready(self):
+        from . import security_signals  # noqa: F401
