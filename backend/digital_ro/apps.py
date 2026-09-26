9154ed6f3456894ec80189b106d4e144321e85b9
akhtarkhan15081983-dotcom <akhtar.khan15081983@gmail.com>
2026-09-26 13:10:56 +0530

    Add Digital RO Passport Django app

---
+++ backend/digital_ro/apps.py
@@ -0,0 +1,7 @@
+from django.apps import AppConfig
+
+
+class DigitalROConfig(AppConfig):
+    default_auto_field = "django.db.models.BigAutoField"
+    name = "digital_ro"
+    verbose_name = "Digital RO Passport"
